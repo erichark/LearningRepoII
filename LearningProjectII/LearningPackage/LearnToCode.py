17fd4f8b2e5f6f1d6cb44e2598e20d2f20c9d46b
erichark <eric.harkrader@gmail.com>
2017-03-04 12:47:13 -0500

    Basic review

--- LearningProjectII/LearningPackage/LearnToCode.py
+++ LearningProjectII/LearningPackage/LearnToCode.py
@@ -3,4 +3,18 @@ Created on Mar 4, 2017
 
 @author: Eric
 '''
-print("will this push?")
+
+
+print(len("this is a string"))
+
+x=1
+y=2
+print(x*y*3)
+
+print (str(x) + "+" + str(y) + str((x+y)))
+
+array1 = [1,2,3,4,5]
+print(array1[1])
+print(array1[3])
+z=array1[1]+array1[3]
+print(z)
