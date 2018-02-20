1f11dce096ee32f1182c22919984695e576e597f
erichark <eric.harkrader@gmail.com>
2018-02-19 22:12:10 -0500

    final updates to finish the lesson

--- webscraper/images.py
+++ webscraper/images.py
@@ -8,23 +8,32 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 from PIL import Image
 import requests
+import os
 
 
 #search_term = input("Input Image Search Term:")
-search_term = "pizza"
+search_term = input("Input search term:")
 params = {"q" : search_term}
+dir_name = search_term.replace(" ", "_").lower()
+
+if not os.path.isdir(dir_name):
+    os.makedirs(dir_name)
+    
 r = requests.get("https://www.bing.com/images/search", params = params)
 print(r)
-
 soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class":"thumbs"})
+links = soup.findAll("a", {"class":"thumb"})
 print(links)
 
 for item in links:
-    img_obj = requests.get(item.attrs["href"])
-    print("Getting:", item.attrs["href"])
-    title = item.attrs.split("/")[-1]
-    img = Image.open(BytesIO(img_obj.content))
-    img.save("./scraped_images" + title, img.format)
-    
-    
+    try:
+        img_obj = requests.get(item.attrs["href"])
+        print("Getting:", item.attrs["href"])
+        title = item.attrs["href"].split("/")[-1]
+        try:
+            img = Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" + title, img.format)
+        except:
+            print("Could not save image.")
+    except:
+        print("could not request image")
