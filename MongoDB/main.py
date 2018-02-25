60546aef049a76325f106c185b604d1170ab3613
erichark <eric.harkrader@gmail.com>
2018-02-25 16:05:54 -0500

    starting mongodb learning

--- MongoDB/main.py
+++ MongoDB/main.py
@@ -4,3 +4,17 @@ Created on Feb 19, 2018
 @author: Eric
 '''
 
+import datetime
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.mydb
+Users = db.users
+
+current_date = datetime.datetime.now()
+
+print (current_date)
+
+olddate = datetime.datetime(2009, 8, 11)
+
