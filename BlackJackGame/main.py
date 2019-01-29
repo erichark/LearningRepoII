06d6dd4f72af386c58fc48caddc7cd037d168d06
erichark <L1meR0ck>
2019-01-29 04:37:17 -0500

    First setup of blackjack program

---
+++ BlackJackGame/main.py
@@ -0,0 +1,28 @@
+'''
+created on 1/28/19
+created by: root
+
+
+player
+    name
+    money
+    position at table
+    dealer flag?
+    cards in hand
+
+card
+    value
+    suit
+
+deck
+    builds a set of 52 cards
+    (Do I want to set up a set of 5 decks, or just randomly generate cards?)
+
+Methods:
+    deal
+    non - player decision/action
+    dealer decision/action
+    calculate money loss/gain
+
+
+'''
