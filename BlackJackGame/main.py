a47afbbedbe1c0d0679dd4cc8683bbe7ef03d5bd
erichark <L1meR0ck>
2019-02-05 21:40:37 -0500

    adding python cheat sheet to gitrepo in non-code repo main.py changes for game setup

--- BlackJackGame/main.py
+++ BlackJackGame/main.py
@@ -1,88 +1,26 @@
 """
 created on 1/28/19
 created by: root
-
-
-player
-    name
-    money
-    position at table
-    dealer flag?
-    cards in hand
-
-card
-    value
-    suit
-
-deck
-    builds a set of 52 cards
-    (Do I want to set up a set of 5 decks, or just randomly generate cards?)
-
-Methods (what do they attach to? which class?):
-    deal
-    non - player decision/action
-    dealer decision/action
-    calculate money loss/gain
-
-
-how to join a player with his hand?
-
-
-Use case - Player setup:
-Instantiate the dealer
-Greet player
-Ask for name
-Instantiate player
-Ask how many people they want to play against
-Instantiate the other players
-
-Use case - initial deal
-deal one card down to all npcs
-deal one card down and tell player thier card
-deal one card down to dealer
-deal one card up to all players (how to handle Aces?)
-
-
-Use case - player has an Ace
-    NPC -
-        if they have a jack - WINNER
-        add cards with Ace value as 11, if sum over 21 ace = 1. if less, and sum is 19-21, stay, if not ace = 1 and hit (?)
-
-    Player -
-        calculate sum, if over 21, make ace = 1, if not, ask player what to do.
-
-    Dealer -
-        calculate sum, if over 21, ace = 1, if 17-21, stay, if under 17, hit.
-
-Use case - playing the game
-    npc players  -
-        if sum is less than 14, hit
-        if sum is 15-16, hit 2 in 3 times
-        if sum 1s 17-18, hit 1 in 3 times
-        if sum is 19+, stand
-        if sum is 21+, tell player is out
-    Player
-        tell player sum
-        ask if they want to hit, or stand
-        if hit, give player new card, tell player sum
-        if over 21, bust, if under
-        repeat
-    Dealer
-        if sum > 17 hit
-        if sum <= 17, stand
-        once dealer stands, compare to each player and indicate winners and losers and increment $$$
-
-Phase II - split/double down/etc?
-
 """
 import random
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class Player:
-    def __init__(self, name, money, table_pos, cards):
+    def __init__(self, name, money, table_pos, hand):
         self.name = name
         self.money = money
         self.table_pos = table_pos
-        self.cards = cards
+        self.hand = hand
 
     def adjust_money(self, bet_result):
         if self.name == "Dealer":
@@ -91,57 +29,119 @@ class Player:
             self.money = self.money + bet_result
 
 
-class NPC (Player):
-    def __init__(self):
-        pass
+class NPC(Player):
+    def __init__(self, name, money, table_pos, hand):
+        Player.__init__(self, name, money, table_pos, hand)
+        if self.name == "Dealer":
+            print(bcolors.BOLD + bcolors.OKGREEN + "{0} initialized.".format(self.name))
+        else:
+            print(bcolors.BOLD + bcolors.OKGREEN+"{0} initialized. ${1}".format(self.name, self.money))
+
 
     def NPCBet(self):
         if self.money > 100:
-            bet = random.randint(1,9) * 10
+            bet = random.randint(1, 9) * 10
         else:
             bet = 10
         return bet
 
 
 class User(Player):
-    pass
+    def __init__(self, name, money, table_pos, hand):
+        Player.__init__(self, name, money, table_pos, hand)
+        print("{0} initialized. ${1}".format(self.name, self.money))
 
-class Dealer(Player):
-    pass
+    def player_bet(self):
+        #Catch exceptions
+        player_bet = input(bcolors.OKBLUE+"How much would you like to bet?"+bcolors.ENDC)
+        return player_bet
 
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
 
-    def cardValue(self):
-        if self.value == "A":
+class Dealer(NPC):
+    def __init__(self, name, money, table_pos, hand):
+        NPC.__init__(self, name, money, table_pos, hand)
+
+
+"""
+class Deck:
+    def __init__(self):
+        for suit in suits:
+            for value in card_list:
+                card = card[suit, value]
+                self.append(card))
+
+    def card_value(self, card):
+        if card.value == "A":
             return 11
-        elif self.value== "K" or "Q" or "J":
+        elif card.value== "K" or "Q" or "J":
             return 10
         else:
-            return str(self.value)
+            return int(card.value)
 
 
-class Deck(Card):
-    def __init__(self):
-        pass
-
-
-"""
-    def Shuffle(self):
-        random.shuffle(self)
+    def Shuffle(self, deck):
+        random.shuffle(deck)
 
 """
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     suits = ("Hearts", "Diamonds", "Spades", "Clubs")
     #suits = ["♠", "♥", "♦", "♣"]
     card_list = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
 
+    """
+        #let's set up the deck
+        #deck = Deck()
+        print(deck[0], deck[13], deck[14], deck[26])
+    """
 
 
-    pass
+    #let's set up the players
+    print(bcolors.BOLD + bcolors.OKGREEN + "Welcome to Blackjack!!!!!!" + bcolors.ENDC)
+    dealer = Dealer("Dealer", 100000, 0, [])
+    npc1 = NPC("Player 1", 10000, 1, [])
+    npc2 = NPC("Player 2", 10000, 2, [])
+
+    name_check = 1
+    while name_check:
+        try:
+            user_name = input("What is your name?")
+            name_check = 0
+        except EOFError:
+            print("I can't use that. Try again.")
+        except KeyboardInterrupt:
+            print("I can't use that. Try again.")
+
+    player = User(user_name, 10000, 3, [])
+    player_list = [dealer, npc1, npc2, player]
+
+    game_exit = 0;
+    while not game_exit:
+        print("Money")
+        for players in player_list:
+            print(bcolors.WARNING + players.name + "     " + "$" + str(players.money) + bcolors.ENDC)
+        print("LET'S PLAY!!!")
+        #player.player_bet()
+
+        play_on = 1
+        while play_on:
+            try:
+                keep_play = input("Would you like to play another hand? (Y or N)")
+                if keep_play == ("N" or "n"):
+                    print("Thanks for playing!!!")
+                    game_exit = 1
+                    play_on = 0
+                else:
+                    game_exit = 0
+                    play_on = 0
+            except EOFError:
+                print("I can't use that. Try again.")
+            except KeyboardInterrupt:
+                print("I can't use that. Try again.")
+
+
+
+
 
 
