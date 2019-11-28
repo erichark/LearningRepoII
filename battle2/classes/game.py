607819a45c62087084073ed60366cf11cd39353f
erichark <L1meR0ck>
2019-11-28 02:52:44 -0500

    Set up player, colors, damage functions,

---
+++ battle2/classes/game.py
@@ -0,0 +1,55 @@
+'''
+
+    Python RPG
+    November 25, 2019
+    Second RPG game
+
+Classes file
+'''
+
+import random
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atk_low = atk - 10
+        self.atk_high = atk + 10
+        self.df = df
+        self.magic = magic
+        self.actions = {"Attack", "Magic"}
+
+    def generate_damage(self):
+        return random.randrange(self.atk_low, self.atk_high)
+
+    def generate_spell_damage(self,i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+
+
+
+
+
+
+
+
--- battle2/main.py
+++ battle2/main.py
@@ -1,3 +1,26 @@
-...
-Python RPG
-November 25, 2019
+'''
+
+    Python RPG
+    November 25, 2019
+    Second RPG game
+
+'''
+
+from classes.game import Person, bcolors
+
+
+magic = [{"name": "Fireball", "cost": 10, "dmg": 50},
+         {"name": "Lightening", "cost": 15, "dmg": 75},
+         {"name": "IcsStorm", "cost": 8, "dmg": 40}
+         ]
+player1 = Person(460, 50, 25, 250, magic)
+
+
+
+print("Player1 hp =", player1.hp)
+spell_dmg = player1.generate_spell_damage(1)
+player1.take_damage(spell_dmg)
+print(spell_dmg)
+print("Player1 hp =", player1.hp)
+
+
