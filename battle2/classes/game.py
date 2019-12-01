5beeb205400eef6b7cd45db0e0cb50b5d81eab25
erichark <L1meR0ck>
2019-12-01 05:48:58 -0500

    first setup of game play, created enemy

--- battle2/classes/game.py
+++ battle2/classes/game.py
@@ -10,14 +10,14 @@ Classes file
 import random
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    PURPLE = '\033[95m'
+    BLUE = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
 
 class Person:
     def __init__(self, hp, mp, atk, df, magic):
--- battle2/main.py
+++ battle2/main.py
@@ -14,15 +14,22 @@ magic = [{"name": "Fireball", "cost": 10, "dmg": 50},
          {"name": "IcsStorm", "cost": 8, "dmg": 40}
          ]
 player1 = Person(460, 50, 25, 250, magic)
+enemy1 = Person(500, 0, 25, 250, magic)
 
+print(bcolors.PURPLE, "Welcome to Dungeon Slayer.", bcolors.ENDC)
+print(bcolors.PURPLE, "A text based RPG by Blix. December 2020.", bcolors.ENDC)
+print(bcolors.YELLOW, bcolors.BOLD, "An enemy ATTACKS!", bcolors.ENDC)
+running = True
+while running:
+    player1.choose_action()
+    action = input("Select Action:")
+    index = int(action) - 1
+    if index == 0:
+        dmg = player1.generate_damage()
+        enemy1.take_damage(dmg)
+        print("The enemy takes", dmg, "points of damage. Enemy HP =", enemy1.get_hp())
 
-
-print("Player1 hp =", player1.hp)
-spell_dmg = player1.generate_spell_damage(1)
-player1.take_damage(spell_dmg)
-print(spell_dmg)
-print("Player1 hp =", player1.hp)
-player1.choose_magic()
-player1.choose_action()
-
+        dmg = enemy1.generate_damage()
+        player1.take_damage(dmg)
+        print("The player takes", dmg, "points of damage. Player HP =", player1.get_hp())
 
